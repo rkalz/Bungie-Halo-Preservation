1ecdc5839f4e246c059b86714654658c62c963e6
rkalz <aleezada@gmail.com>
2018-07-26 20:07:23 -0500

    Fixed Elasticsearch upload script. Added index example.

--- upload_to_elasticsearch.py
+++ upload_to_elasticsearch.py
@@ -1,24 +1,24 @@
 from credentials import ES_URL
+from math import floor
+from requests import post
 import json
 import os
 import random
-import requests
 
 
 def upload_to_elasticsearch():
     path_to_folder = "C:\\Users\\aleez\\Desktop\\Halo 2 Data\\fixed_files\\"
     files_to_upload = os.listdir(path_to_folder)
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    for _ in range(9999):
+    for _ in range(9000):
         file = random.choice(files_to_upload)
-        id = file[file.rfind('_')+1:file.rfind('.')]
+        game_id = file[file.rfind('_')+1:file.rfind('.')]
         with open(path_to_folder + file, 'r') as f:
-            data = json.loads(f.read())
-            requests.post(ES_URL + "/h2/" + id, json=data, headers=headers)
-            print(id)
+            match_data = json.loads(f.read())
+            r = post(ES_URL + "/h2/_doc/" + game_id, json=match_data)
+            print(game_id + ": " + str(r.status_code))
+            if floor(r.status_code / 100) != 2:
+                print(r.content)
+                break
 
 
 upload_to_elasticsearch()
