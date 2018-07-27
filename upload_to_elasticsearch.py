8df7b2d6a859951d61181d8692ef74fecd8a5133
rkalz <aleezada@gmail.com>
2018-07-26 20:22:02 -0500

    Fixed bad index script

--- upload_to_elasticsearch.py
+++ upload_to_elasticsearch.py
@@ -9,7 +9,7 @@ import random
 def upload_to_elasticsearch():
     path_to_folder = "C:\\Users\\aleez\\Desktop\\Halo 2 Data\\fixed_files\\"
     files_to_upload = os.listdir(path_to_folder)
-    for _ in range(9000):
+    for i in range(9994):
         file = random.choice(files_to_upload)
         game_id = file[file.rfind('_')+1:file.rfind('.')]
         with open(path_to_folder + file, 'r') as f:
@@ -18,6 +18,7 @@ def upload_to_elasticsearch():
             print(game_id + ": " + str(r.status_code))
             if floor(r.status_code / 100) != 2:
                 print(r.content)
+                print(str(10000-i) + " files remaining")
                 break
 
 
